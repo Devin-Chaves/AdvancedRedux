f443ddc384baa82bb6d60220e487eb20b3d3211e
Devin Chaves <dchav90@gmail.com>
2017-10-12 13:38:16 -0400

    User List component

---
+++ middlewares/src/components/user_list.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class UserList extends Component {
+  renderUser(user) {
+    return (
+      <div className="card card-block">
+        <h4 className="card-title">{user.name}</h4>
+        <p className="card-text">Cheese Factory</p>
+        <a className="btn btn-primary">Email</a>
+      </div>
+    );
+  }
+  render() {
+    return <div>{this.props.user.map(this.renderUser)}</div>;
+  }
+}
