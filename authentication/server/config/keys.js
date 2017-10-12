e38dc758987ae6ef69891504a547313a1daa7537
Devin Chaves <dchav90@gmail.com>
2017-10-12 18:18:18 -0400

    mLab setup

---
+++ authentication/server/config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb://dchaves:Blake8756@ds119565.mlab.com:19565/auth-dev'
+};
--- authentication/server/index.js
+++ authentication/server/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
 const router = require('./router');
+require('./models/user');
+const keys = require('./config/keys');
+
+mongoose.connect(keys.mongoURI);
 
 // App Setup
 app.use(morgan('combined'));
